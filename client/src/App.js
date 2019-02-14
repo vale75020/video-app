150e197ff0dd4d02b54704e6077e6d940f146873
vale75020 <valentinosuma@hotmail.com>
2019-02-14 17:10:37 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+
+const port = 5000
+
+var morgan = require ('morgan');
+
+app.use(morgan('tiny'));
+
+const mongoose = require('mongoose'); // doc mongoose pour connecter mongo
+mongoose.connect('mongodb://localhost:27017/video-manager', {useNewUrlParser: true});
+
+let db = mongoose.connection;  // doc mongoose pour verifier la connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("connected to mongodb");
+});
+
+let Schema = mongoose.Schema; // doc mongoose schema
+let VideoSchema = new Schema({
+    title:  String,
+    description: String,
+    hashtags: String,
+    url:   String 
+  });
+
+let SerieSchema = new Schema({
+    videos: [VideoSchema],
+});
+
+let Video = mongoose.model("Video", VideoSchema);
+let Serie = mongoose.model("Serie", SerieSchema);
+
+// let v = new Video({
+//     title:  "My first video title",
+//     description: "a video test for my DB",
+//     hashtags: "my hashtag",
+//     url:   "http://myfirstvideo.com" 
+// });
+
+// v.save();
+
+// Video.find(function(err, docs){
+//   console.log(docs)
+// })
+
+app.get('/', function (req, res) {
+
+  res.send('Hello World!')
+})
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`)
+})
